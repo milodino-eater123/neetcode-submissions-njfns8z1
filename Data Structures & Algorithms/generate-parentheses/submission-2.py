deb54386640863fbd9bc6f471799cee6d2c054f7
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-12 17:12:21 +0800

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def bracket(string,opens,closes):
+            nonlocal res
+            if opens == n and closes == n:
+                res.append(string)
+                
+
+            if opens < n:
+                bracket(string+"(",opens+1,closes)
+            
+            if opens > closes:
+                bracket(string+")",opens,closes+1)
+        bracket("",0,0)
+        return res
+        
