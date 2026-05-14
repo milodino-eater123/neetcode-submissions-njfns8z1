0303b9d015288dad27d3faf89d7a35e3d9a366cc
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-14 11:39:59 +0800

    Add: islands-and-treasure - submission-5

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-5.py
@@ -0,0 +1,35 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        n,m = len(grid),len(grid[0])
+        stack = []
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]==0:
+                    stack.append((i,j,0))
+        
+        while stack:
+            i,j,dist = stack.pop()
+            if i<0 or j<0 or i>=n or j>=m:
+                continue
+            if grid[i][j] == -1:
+                continue
+
+            if grid[i][j] < dist:
+                continue
+            grid[i][j] = dist
+            
+            stack.extend([
+    (i-1, j, dist+1),
+    (i+1, j, dist+1),
+    (i, j-1, dist+1),
+    (i, j+1, dist+1)
+])
+
+
+        
+
+        
+        
+
+        
