50e7b737133275a16eeaa437dee964665b655421
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-15 08:03:24 +0800

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        stations=0
+        fuel = 0
+        maxStations = 0
+        for i in range(2):
+          for i in range(len(gas)):
+            g=gas[i]
+            c=cost[i]
+            fuel += g
+            if fuel<c:
+                start = i+1
+                stations = 0
+                fuel = 0
+            else:
+                stations+=1
+                maxStations = max(maxStations,stations)
+                fuel-=c
+        print(stations)
+        return start if stations>=len(gas) else -1
+            
+        
