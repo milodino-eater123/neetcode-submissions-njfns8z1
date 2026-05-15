96d65803e992a294a759f02a5a98761844d66c7a
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-15 15:49:11 +0800

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        hmap = defaultdict(int)
+        for n in hand:
+            hmap[n] += 1
+        
+        hand = list(set(hand))
+        hand.sort()
+        
+        for n in hand:
+            freq = hmap[n]       
+            for i in range(1,groupSize):
+                hmap[n+i] -= freq
+                if hmap[n+i] < 0:
+                    return False
+        
+        return True
+        
+
+        
