7f2367b7d980ae319520edaa90f2bd8fabf20f18
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-24 11:51:00 +0800

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+        hmap = {}
+        def dfs(i,j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i,j) in hmap:
+                return hmap[(i,j)]
+
+            res = 0
+            if s[i] == t[j]:
+                res += dfs(i+1,j+1)
+            
+            res += dfs(i+1,j)
+            
+            hmap[(i,j)] = res
+            return res
+
+        return dfs(0,0)
+        
+
+        
