ced35315925d5dd92fe830fdafac714f436827b0
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-29 22:32:41 +0800

    Add: jump-game-ii - submission-9

---
+++ Data Structures & Algorithms/jump-game-ii/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return 0
+        originalReach = nums[0]
+        newReach = 0
+        res = 0
+        for i,n in enumerate(nums):
+            if i>originalReach:
+                res += 1
+                originalReach=newReach
+            newReach = max(newReach,i+n)
+        return res + 1
+            
+        
