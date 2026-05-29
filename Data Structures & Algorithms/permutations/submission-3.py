9c9ac116af5fe5152bc9500c51da76fc256917d9
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-29 19:37:50 +0800

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def math(path,used):
+            if len(path)==len(nums):
+                res.append(path.copy())
+                return
+            
+            for n in nums:
+                if n in used:
+                    continue
+                path.append(n)
+                used.add(n)
+                math(path,used)
+                path.pop()
+                used.remove(n)
+
+        math([],set())
+        return res
+
+        
