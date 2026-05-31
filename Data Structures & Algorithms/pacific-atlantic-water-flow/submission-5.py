236126d7c539253bffaee9cc328f8af62683bcff
milodino-eater123 <samuelkaien1@gmail.com>
2026-05-31 18:12:06 +0800

    Add: pacific-atlantic-water-flow - submission-5

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        n,m = len(heights),len(heights[0])
+        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+        def bfs(stack,sea):
+            while stack:
+                newStack = set()
+                while stack:
+                    x,y = stack.pop()
+                    sea.add((x,y))
+                    for dx,dy in dirs:
+                        i,j = x+dx,y+dy
+                        if i<0 or j<0 or i==n or j==m or (i,j) in sea:
+                            continue
+                        if heights[i][j]<heights[x][y]:
+                            continue
+                        newStack.add((i,j))
+                stack=list(newStack)
+        
+        inPacific,inAtlantic = set(),set()
+
+        stack1 = [(0,j) for j in range(m)] + [(i,0) for i in range(1,n)]
+        stack2 = [(n-1,j) for j in range(m)] + [(i,m-1) for i in range(n-1)]
+
+        bfs(stack1,inPacific)
+        bfs(stack2,inAtlantic)
+
+        res = []
+        for coord in inPacific:
+            if coord in inAtlantic:
+                res.append(coord)
+        return res
+
+
+                    
+
+                
+            
