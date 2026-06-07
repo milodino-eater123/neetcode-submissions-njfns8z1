e0b27d16dfeb3929814129af50bfd342a17fc80e
milodino-eater123 <samuelkaien1@gmail.com>
2026-06-07 22:16:51 +0800

    Add: edit-distance - submission-9

---
+++ Data Structures & Algorithms/edit-distance/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        l1,l2 = len(word1),len(word2)
+        dp = [] 
+        for j in range(l2+1):
+            dp.append(l2-j)
+        for i in range(l1-1,-1,-1):
+            placeholder=l1-i-1
+            dp[l2]=l1-i
+            for j in range(l2-1,-1,-1):
+                temp=dp[j]
+                if word1[i]==word2[j]:
+                    dp[j]=placeholder
+                else:
+                    dp[j]=1+min(dp[j],dp[j+1],placeholder)
+                placeholder=temp
+        return dp[0]
+
+
+
+          
+        
+
