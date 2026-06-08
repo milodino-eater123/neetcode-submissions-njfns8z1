6d4411adbac90236c80dfe8a88615e339c253e6c
milodino-eater123 <samuelkaien1@gmail.com>
2026-06-08 08:13:58 +0800

    Add: longest-increasing-path-in-matrix - submission-6

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        n,m=len(matrix),len(matrix[0])
+        hmap,visited,res={},set(),0
+        def dfs(i,j,prev):
+            if i<0 or j<0 or i==n or j==m:
+                return 0
+            if matrix[i][j]<=prev:
+                return 0
+            if (i,j) in hmap:
+                return hmap[(i,j)]
+            cur=matrix[i][j]
+            best = 1 + max(dfs(i+1,j,cur),dfs(i-1,j,cur),dfs(i,j+1,cur),dfs(i,j-1,cur))
+            hmap[(i,j)]=best
+            return best
+        for i in range(n):
+            for j in range(m):
+                res=max(res,dfs(i,j,float("-inf")))
+        return res
