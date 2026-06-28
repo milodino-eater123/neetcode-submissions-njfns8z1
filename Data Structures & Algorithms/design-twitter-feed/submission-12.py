99ac3261df69c2f7a1f80e2c884aa938faef4457
milodino-eater123 <samuelkaien1@gmail.com>
2026-06-28 19:50:41 +0800

    Add: design-twitter-feed - submission-12

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-12.py
@@ -0,0 +1,36 @@
+from collections import defaultdict,deque
+from heapq import heappush,heappop
+class Twitter:
+    def __init__(self):
+        self.tweets=defaultdict(deque)
+        self.followed=defaultdict(set)
+        self.time=0
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append((self.time,tweetId))
+        self.time-=1
+        if len(self.tweets)>10:
+            self.tweets.popleft()
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res,minHeap = [],[]
+        followed = self.followed[userId]|{userId}
+        for followee in followed:
+            dq = self.tweets[followee]
+            if dq:
+                time,id=dq[-1]
+                heappush(minHeap,(time,id,followee,len(dq)-2))
+        while minHeap and len(res)<10:
+            _,id,user,j=heappop(minHeap)
+            res.append(id)
+            if j>=0:
+                time,id=self.tweets[user][j]
+                heappush(minHeap,(time,id,user,j-1))
+        return res
+
+        
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followed[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followed[followerId].discard(followeeId)
+        
